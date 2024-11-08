5604e4d8dbc82641b19c2081f8b47c03f46b67dc
hst1189 <hst1189@hotmail.com>
2024-11-08 20:30:18 +0900

    Update check_m3u8.sh

--- check_m3u8.sh
+++ check_m3u8.sh
@@ -1,16 +1,20 @@
 #! /usr/bin/env bash
 
+dirwork=""
 function read_dir(){
-    for file in `ls $1`
-    do
-        if [ -d $1"/"$file ]  //注意此处之间一定要加上空格，否则会报错
-        then
-            read_dir $1"/"$file
-        else
-            echo $1"/"$file
+  for file in `ls $1`
+  do
+    if [ -d $1"/"$file ]; then
+        echo "―folder: "$file
+        if [ ! -d ${GITHUB_WORKSPACE}/local/api/$file ]; then
+            mkdir -p ${GITHUB_WORKSPACE}/local/api/$file
         fi
-    done
+        dirwork=$file"/"
+        read_dir $1"/"$file
+    else
+        echo "⊥file: "$file
+        iptv-checker -o ${GITHUB_WORKSPACE}/local/output -p 100 -t 60000 $1"/"$file
+        cp -pr ${GITHUB_WORKSPACE}/local/output/online.m3u ${GITHUB_WORKSPACE}/local/api/$dirwork$file
+    fi
+  done
 }
-#测试目录 test
-read_dir test
-
